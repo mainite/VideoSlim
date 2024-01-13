4dfa0daf62202d0795d1160a69fb7b5baae34838
inite <main@inite.cn>
2024-01-13 15:06:03 +0800

    修复音频问题，通过GitHub的Releases进行版本检测

--- VideoSlim/VideoSlim.py
+++ VideoSlim/VideoSlim.py
@@ -1,14 +1,22 @@
 # Copyright (c) <2023> <hotMonk> <inite.cn>
 
 from tkinter import *
-import os
 import webbrowser
 import windnd
+from tkinter import messagebox
+from moviepy.editor import *
+import requests
 
 class DragDropApp():
     def __init__(self, root):
+
+        #定义当前版本号
+        self.Version_number = 'v1.4'
+
+
+
         self.root = root
-        self.root.title("VideoSlim 视频压缩 v1.4")
+        self.root.title("VideoSlim 视频压缩  "+self.Version_number)
         self.root.resizable(width=False, height=False)
         self.root.iconbitmap(os.path.join(os.getcwd(), "./tools/icon.ico"))
         screenwidth = self.root.winfo_screenwidth()
@@ -45,8 +53,13 @@ class DragDropApp():
 
         # 选择框
         self.delete_source_var = BooleanVar()
-        delete_source_check = Checkbutton(self.root, text="完成后删除旧文件", variable=self.delete_source_var, onvalue=True, offvalue=False)
-        delete_source_check.place(x=20, y=295)
+        delete_source_check = Checkbutton(self.root, text="完成后删除旧文件", variable=self.delete_source_var,
+                                          onvalue=True, offvalue=False)
+        delete_source_check.place(x=20, y=287)
+
+        self.delete_audio_var = BooleanVar()
+        delete_audio_check = Checkbutton(self.root, text="删除音频轨道", variable=self.delete_audio_var, onvalue=True,offvalue=False)
+        delete_audio_check.place(x=20, y=313)
 
         # 拖拽控件
         windnd.hook_dropfiles(self.root, func=self.drop_file)
@@ -68,28 +81,55 @@ class DragDropApp():
         text_content = self.text_box.get("1.0", END)
         if text_content != "":
             lines = text_content.splitlines()
+            if len(lines) <= 1:
+                messagebox.showwarning("提示", "请先拖拽文件到此处")
+                return False
+
             for file_name in lines:
                 if file_name != "":
                     save_out_name = self.GetSaveOutFileName(file_name)
-                    command1 = r'.\tools\ffmpeg.exe -i "{}" -vn -sn -v 0 -c:a pcm_s16le -f wav pipe: | .\tools\neroAacEnc.exe -ignorelength -lc -br 128000 -if - -of ".\old_atemp.mp4"'
-                    command2 = r'.\tools\x264_32-8bit.exe --crf 23.5 --preset 8 -I 600 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o ".\old_vtemp.mp4"  "{}"'
-                    command3 = r'.\tools\mp4box.exe -add ".\old_vtemp.mp4#trackID=1:name=" -add ".\old_atemp.mp4#trackID=1:name=" -new "{}"'
-                    command4 = "del .\\old_atemp.mp4 .\\old_vtemp.mp4"
-                    command5 = r'del "{}"'
+                    # 判断视频是否拥有音频轨道
+                    video = VideoFileClip(file_name)
+                    if video.audio is None or self.delete_audio_var.get():
+                        print("视频没有音频轨道")
+                        command1 = r'.\tools\x264_32-8bit.exe --crf 23.5 --preset 8 -I 900 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o "{}"  "{}"'
+                        os.system(command1.format(save_out_name, file_name))
 
-                    os.system(command1.format(file_name))
-                    os.system(command2.format(file_name))
-                    os.system(command3.format(save_out_name))
-                    os.system(command4)
+                        if self.delete_source_var.get():
+                            os.system(command5.format(file_name))
 
-                    if self.delete_source_var.get():
-                        os.system(command5.format(file_name))
-            print("Completed Successfully ！！！")
-        else:
-            print("No File")
+                    else:
+                        print("视频有音频轨道")
+                        command1 = r'.\tools\ffmpeg.exe -i "{}" -vn -sn -v 0 -c:a pcm_s16le -f wav pipe: | .\tools\neroAacEnc.exe -ignorelength -lc -br 128000 -if - -of ".\old_atemp.mp4"'
+                        command2 = r'.\tools\x264_32-8bit.exe --crf 23.5 --preset 8 -I 600 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o ".\old_vtemp.mp4"  "{}"'
+                        command3 = r'.\tools\mp4box.exe -add ".\old_vtemp.mp4#trackID=1:name=" -add ".\old_atemp.mp4#trackID=1:name=" -new "{}"'
+                        command4 = "del .\\old_atemp.mp4 .\\old_vtemp.mp4"
+                        command5 = r'del "{}"'
+
+                        os.system(command1.format(file_name))
+                        os.system(command2.format(file_name))
+                        os.system(command3.format(save_out_name))
+                        os.system(command4)
+
+                        if self.delete_source_var.get():
+                            os.system(command5.format(file_name))
+            # 弹出信息框
+            messagebox.showinfo("提示", "转换完成")
+
+    def version_number_detection(self):
+        url = f"https://api.github.com/repos/mainite/VideoSlim/releases"
+        response = requests.get(url)
+        data = response.json()
+        if data and len(data):
+            latest_release = data[0]  # 第一个元素就是最新的Release
+
+            if latest_release != self.Version_number:
+                messagebox.showinfo("更新提示", "有新版本可用，请前往官网更新")
+
 
 
 if __name__ == '__main__':
     root = Tk()
     app = DragDropApp(root)
+    app.version_number_detection()
     root.mainloop()
