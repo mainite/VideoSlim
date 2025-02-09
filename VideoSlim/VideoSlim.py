36f51e4805ec548fd2d2e41802c9a103e3db49c3
inite <main@inite.cn>
2025-02-09 22:24:20 +0800

    🚀update:version-1.7

--- VideoSlim/VideoSlim.py
+++ VideoSlim/VideoSlim.py
@@ -150,7 +150,7 @@ class DragDropApp():
     def __init__(self, root):
 
         # 定义当前版本号
-        self.Version_number = 'v1.6'
+        self.Version_number = 'v1.7'
 
         self.root = root
         self.root.title("VideoSlim 视频压缩  " + self.Version_number)
